2ad49463b5baf615f868b48185ab635d2983c769
Apeksha-P <pavithriapeksha1new@gmail.com>
2023-12-01 17:09:47 +0530

    Create LibrarySystem class

--- src/LibrarySystem.java
+++ src/LibrarySystem.java
@@ -97,8 +97,18 @@ class Library{
         System.out.println("Total number of items in the library: "+borrowableItemsList.size());
     }
 }
-public class Main {
+public class LibrarySystem {
     public static void main(String[] args) {
-
+        Book book1 = new Book("Seena Maragatham","Sujatha Thilaka", "9789553023975");
+        Book book2 = new Book("Nuramakadya Bauthika Nuladanayakshanamaala", "Seynamasasekaka","9789553548721");
+        Book book3 = new Book("Island of a Thousand Mirrors","Nayomi Munaweera", "9781616953623");
+        Library library = new Library();
+        library.addLibraryItem(book1);
+        library.addLibraryItem(book2);
+        library.addLibraryItem(book3);
+        library.displayLibraryInfo();
+        library.listAvailableItems();
+        library.checkoutItem("Island of a Thousand Mirrors");
+        library.listAvailableItems();
     }
 }
